4089845de3550ea46713c3fca6447a076cfba396
Rob Coward <rob@jive-videos.net>
2017-03-29 11:35:06 +0100

    OS-142 Added instructions for launching ELK deployment

--- logstash/scripts/poll-metrics.sh
+++ logstash/scripts/poll-metrics.sh
@@ -2,4 +2,4 @@
 #
 
 echo "{\"vcd_api_url\": \"https://${VCD_API_HOST}/api\", \"vcd_username\": \"${VCD_USERNAME}@${VCD_ORG}\", \"vcd_password\": \"${VCD_PASSWORD}\" }" | \
-  curl -X POST -d @- http://${VCLOUD_METRICS_SERVICE_HOST}:${VCLOUD_METRICS_SERVICE_PORT}/stats
+  curl -X POST -d @- http://${METRICS_SERVICE_HOST}:${METRICS_SERVICE_PORT}/stats
